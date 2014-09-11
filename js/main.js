00439c06e1013d6c5afab3ec1b761025c1f8c858
John Hollén <johnhollen982@gmail.com>
2014-09-11 10:43:05 +0200

    Added sideMenu and changed some design and cameraControls

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 // SETUP SCENE ********************
 //*********************************
 var height = 500;
-var width = 980;
+var width = 880;
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, width/height, 0.1, 1000);
@@ -17,7 +17,7 @@ controls.addEventListener('change', render);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(width, height);
 renderer.setClearColor( 0x99ccff, 1);
-document.getElementById("container").appendChild(renderer.domElement);
+document.getElementById("canvasContainer").appendChild(renderer.domElement);
 
 //Add lights
 var light = new THREE.PointLight(0xffffff, 1, 0);
@@ -28,36 +28,36 @@ scene.add(light);
 scene.add(ambientLight);*/
 
 //Load car
-var manager = new THREE.LoadingManager();
+/*var manager = new THREE.LoadingManager();
 manager.onProgress = function (item, loaded, total) {
     console.log(item, loaded, total);
-};
+};*/
 
 // plane
-var grassTexture = THREE.ImageUtils.loadTexture("texture/grass_texture.jpg");
+/*var grassTexture = THREE.ImageUtils.loadTexture("texture/grass_texture.jpg");
 grassTexture.wrapS = THREE.RepeatWrapping;
-grassTexture.wrapT = THREE.RepeatWrapping;
+grassTexture.wrapT = THREE.RepeatWrapping;*/
 
 //Set repeat how many times
-grassTexture.repeat.set(4,4);
+/*grassTexture.repeat.set(4,4);*/
 
-var plane = new THREE.Mesh(new THREE.PlaneGeometry(700,700, 50, 50), new THREE.MeshLambertMaterial({map : grassTexture}));
+var plane = new THREE.Mesh(new THREE.PlaneGeometry(700,700, 50, 50), new THREE.MeshLambertMaterial({color: 0xffffff}));
 //plane.rotation.z += 45*(Math.PI/180);
 plane.rotation.x -= 90*(Math.PI/180);
 plane.position.y = -5;
 scene.add(plane);
 
 //Texture
-var texture = new THREE.Texture();
+/*var texture = new THREE.Texture();
 
 var loader = new THREE.ImageLoader( manager );
 loader.load('obj/policeCar/0000.BMP', function(image){
     texture.image = image;
     texture.needsUpdate = true;
-});
+});*/
 
 //Model
-var loader = new THREE.OBJLoader( manager );
+/*var loader = new THREE.OBJLoader( manager );
 
 loader.load('obj/policeCar/crown_victoria.obj', function(object){
     object.traverse(function(child){
@@ -103,7 +103,7 @@ loader.load('obj/RaceTrack/FullTrack.obj', function(object){
     object.scale.set(90,90,90);
     object.castShadow = true;
     scene.add(object);
-});
+});*/
 
 
 animate();
